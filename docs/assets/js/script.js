e183c7ad041060954d47cea649e13ea0e89325b3
reguv760 <reguv760@gmail.com>
2017-09-12 17:19:01 -0700

    build test v1

---
+++ docs/assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+{
+
+    // init mobile button nav
+    $(".button-collapse").sideNav();
+     $('select').material_select();
+
+    console.log("document ready!");
+
+});
+
